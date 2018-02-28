fe382f667bfe2e932ee829dc9aee1ab68fe9585a
Christopher Etges <Chris@MacBook-Pro.local>
2018-02-27 18:23:55 -0700

    roughBuild

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+//user clicks button to start game
+
+//game displays a question with 4 answers
+
+    //questions are stored in an array of objects, each object will have the question and the associated answers
+
+//timer starts after question and answers are displayed
+
+//user clicks on answer
+    //if right, display you are correct message and add to score
+
+    //if wrong, display you are wrong and correct answer, no change to score or deduct score
+
+    //if user doesn't click on anything, display same thing as above, maybe tweaking the message to reflect that they ran out of time
+
+//this loop runs until there are no more questions
