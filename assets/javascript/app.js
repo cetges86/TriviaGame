02a542adeac266afa310b055e5347614a7e993f1
Christopher Etges <Chris@MacBook-Pro.local>
2018-03-05 19:27:05 -0700

    bootstrap,css,html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,22 +3,22 @@ $(document).ready(function () {
     //user clicks button to start game
     $('#start').click(function () {
         //questions and answers, stored in an array full of objects
-        $('#start').html('');
-
+        $('#start').hide();
         var index = 0;
-        var time = 15;
+        var time = 16;
         var score = 0;
-
+        console.log(this);
         //game displays a question with 4 answers
 
         function displayQuestion() {
             $('#message').empty();
             $('#gif').empty();
+            $('#display').show();
             $('#question').html(data.questions[index].question);
-            $('#answers').html('<li>A: ' + data.questions[index].answers.a + '</li><br>');
-            $('#answers').append('<li>B: ' + data.questions[index].answers.b + '</li><br>');
-            $('#answers').append('<li>C: ' + data.questions[index].answers.c + '</li><br>');
-            $('#answers').append('<li>D: ' + data.questions[index].answers.d + '</li>');
+            $('#answers').html('<button class="btn btn-info"><strong>A - </strong> ' + data.questions[index].answers.a + '</button><br>');
+            $('#answers').append('<button class="btn btn-info"><strong>B - </strong> ' + data.questions[index].answers.b + '</button><br>');
+            $('#answers').append('<button class="btn btn-info"><strong>C - </strong> ' + data.questions[index].answers.c + '</button><br>');
+            $('#answers').append('<button class="btn btn-info"><strong>D - </strong> ' + data.questions[index].answers.d + '</button>');
             ready();
             //timer starts after question and answers are displayed
         };
@@ -36,7 +36,7 @@ $(document).ready(function () {
 
         //user clicks on answer
         function ready() {
-            $('li').click(function (event) {
+            $('button').click(function (event) {
                 var userAnswer = event.target.textContent;
                 if (userAnswer === data.questions[index].correct) {
                     questionRight();
@@ -49,7 +49,8 @@ $(document).ready(function () {
         //if right, display you are correct message and add to score
         function questionRight() {
             $('#timer').empty();
-            $('#message').html('<h1>You are correct!</h1>');
+            $('#display').hide();
+            $('#message').html('<h1>You are correct! Way to go!</h1>');
             displayGif();
             score++;
             index++;
@@ -57,31 +58,32 @@ $(document).ready(function () {
                 setTimeout(gameOver, 5000);
             } else {
                 setTimeout(displayQuestion, 5000);
-                time = 20;
+                time = 21;
             };
         };
 
         function questionWrong() {
             $('#timer').hide();
-            $('#message').html('<h1>Sorry! The correct answer was: </h1><br><h2>' + data.questions[index].correct + '</h2>');
+            $('#display').hide();
+            $('#message').html('<h1>Oh no! The correct answer was: </h1><br><h2>' + data.questions[index].correct + '</h2>');
             displayGif();
             index++;
             if (index >= data.questions.length) {
                 gameOver(score);
             } else {
                 setTimeout(displayQuestion, 5000);
-                time = 20;
+                time = 21;
             };
         };
 
         function gameOver() {
-            var finalScore = ((score / 7) * 100).toFixed(2);
+            var finalScore = ((score / data.questions.length) * 100).toFixed(2);
             $('#question').empty();
             $('#answers').empty();
             $('#timer').empty();
             clearInterval(startTimer);
-            $('#gif').html(data.gifs[7]);
-            $('#message').html(`<h1>Your final score was: ${score} out of 7, or ${finalScore} %</h1>`)
+            $('#gif').html(data.gifs[9]);
+            $('#message').html(`<h1>Your final score was: ${score} out of ${data.questions.length}, or ${finalScore} %</h1>`);
 
         }
 
@@ -93,16 +95,5 @@ $(document).ready(function () {
         if (index < data.questions.length) {
             setInterval(startTimer, 1000);
         }
-
-
-
-
-
-
-        //if wrong, display you are wrong and correct answer, no change to score or deduct score
-
-        //if user doesn't click on anything, display same thing as above, maybe tweaking the message to reflect that they ran out of time
-
-        //this loop runs until there are no more questions
     });
 });
--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -8,7 +8,7 @@ var data = {
                     "c": "Femur",
                     "d": "Clavicle"
                 },
-                "correct": "C: Femur"
+                "correct": "C -  Femur"
             },
             {
                 "question": "How many types of photoreceptors are present in your retinas, and what are they called?",
@@ -18,7 +18,7 @@ var data = {
                     "c": "Two - Spheroid and Cylindrical",
                     "d": "One - Light"
                 },
-                "correct": "A: Two - Rods and Cones"
+                "correct": "A -  Two - Rods and Cones"
             },
             {
                 "question": "Which lobe (region) of your brain is responsible for complex thinking and problem solving?",
@@ -28,7 +28,7 @@ var data = {
                     "c": "Parietal Lobe",
                     "d": "Frontal Lobe"
                 },
-                "correct": "D: Frontal Lobe"
+                "correct": "D -  Frontal Lobe"
             },
             {
                 "question": "In which area of the body would you find the scapula?",
@@ -38,7 +38,7 @@ var data = {
                     "c": "Hand",
                     "d": "Thigh"
                 },
-                "correct": "B: Shoulder"
+                "correct": "B -  Shoulder"
             },
             {
                 "question": "What is the largest muscle in the human body?",
@@ -48,7 +48,7 @@ var data = {
                     "c": "Gastrocnemius",
                     "d": "Gluteus Maximus"
                 },
-                "correct": "D: Gluteus Maximus"
+                "correct": "D -  Gluteus Maximus"
             },
             {
                 "question": "What is the largest organ in the human body?",
@@ -58,7 +58,7 @@ var data = {
                     "c": "Brain",
                     "d": "Small Intestine"
                 },
-                "correct": "B: Skin"
+                "correct": "B -  Skin"
             },
             {
                 "question": "What is the technical name for your kneecap?",
@@ -68,8 +68,38 @@ var data = {
                     "c": "Pelvis",
                     "d": "Patella"
                 },
-                "correct": "D: Patella"
-            }
+                "correct": "D -  Patella"
+            },
+            {
+                "question": "What is the name of the upper arm bone?",
+                "answers": {
+                    "a": "Ulna",
+                    "b": "Humerus",
+                    "c": "Clavicle",
+                    "d": "Tibia"
+                },
+                "correct": "B -  Humerus"
+            },
+            {
+                "question": "What does the term 'hypoxia' mean?",
+                "answers": {
+                    "a": "An excess of oxygen",
+                    "b": "A low amount of oxygen",
+                    "c": "An absence of carbon dioxide",
+                    "d": "A low amount of water"
+                },
+                "correct": "B -  A low amount of oxygen"
+            },
+            {
+                "question": "What is the primary function of your liver?",
+                "answers": {
+                    "a": "Breaks down chemicals and drugs in your bloodstream",
+                    "b": "Filters sugar out of urine",
+                    "c": "Remove carbon dioxide from your blood",
+                    "d": "Produce and release insulin"
+                },
+                "correct": "A -  Breaks down chemicals and drugs in your bloodstream"
+            },
         ],
         "gifs": [
             "<iframe src='https://giphy.com/embed/GeWjRVP3UTXyM' width='480' height='269' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>",
@@ -79,6 +109,9 @@ var data = {
             "<iframe src='https://giphy.com/embed/FBMOHoG4L5g4' width='480' height='270' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>",
             "<iframe src='https://giphy.com/embed/3o6Mbd7jEEv2SbWoz6' width='480' height='368' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>",
             "<iframe src='https://giphy.com/embed/y335xyKYQGrja' width='480' height='240' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>",
+            "<iframe src='https://giphy.com/embed/4xpB3eE00FfBm' width='480' height='458' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>",
+            "<iframe src='https://giphy.com/embed/ncsQI9sF3pa8w' width='480' height='443' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>",
+            "<iframe src='https://giphy.com/embed/l2Jec66NKREEw7JsY' width='480' height='368' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>",
             "<iframe src='https://giphy.com/embed/SIPIe590rx6iA' width='480' height='480' frameBorder='0' class='giphy-embed' allowFullScreen></iframe>"
         ]
     }
